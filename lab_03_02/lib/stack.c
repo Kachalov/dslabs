efb91b510d74ee5e429e88c571dd903d914c8a7c
Kachalov Alexey <kachalov@kistriver.com>
2017-10-23 00:49:29 +0300

    Added stack functions

---
+++ lab_03_02/lib/stack.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "stack.h"
+#include "errors.h"
+
+int stack_push(stack_t *s)
+{
+
+}
+
+int stack_pop(stack_t *s)
+{
+
+}
+
+int stack_init(stack_t *s, void *lp, stack_type_t type, size_t size)
+{
+
+}
--- lab_03_02/stack.h
+++ lab_03_02/stack.h
@@ -1,8 +1,18 @@
 #pragma once
 
+#include <inttypes.h>
+#include <stdbool.h>
+
 typedef struct
 {
+    uint8_t up:1;
+    uint8_t list:1;
     void *lp;
     void *hp;
     void *sp;
 } stack_t;
+
+int stack_push(stack_t *s);
+int stack_pop(stack_t *s);
+
+int stack_init(stack_t *s, void *lp, bool up, bool list, size_t offset);
